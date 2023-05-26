bc33244207b03e48c2a117ad2d36dccc3128e022
Jagnito Studio <jagnito.studio.au@gmail.com>
2023-05-26 23:33:53 +1000

    Initial Commit

---
+++ src/Test.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+function Test() {
+  const nameList = []
+  return (
+    <div>
+      <div className="grid grid-cols-4">
+        <div></div>
+        <div className='col-span-2'>
+          <div className="grid grid-cols-3 gap-4">
+              <div className='flex flex-col items-center'>
+                <img className='w-24 h-24' src="https://media.licdn.com/dms/image/C5603AQHCQsuLbeNEKw/profile-displayphoto-shrink_800_800/0/1517754283443?e=1689811200&v=beta&t=SaCZeBy4QS_hRL0xId3a8vu3icAlE85Qps7B2J3uzaQ" alt="" />
+                <div>Nishant MOHAN</div>
+              </div>
+
+              <div className='flex flex-col items-center'>
+                <img className='w-24 h-24' src="https://media.licdn.com/dms/image/C5603AQHCQsuLbeNEKw/profile-displayphoto-shrink_800_800/0/1517754283443?e=1689811200&v=beta&t=SaCZeBy4QS_hRL0xId3a8vu3icAlE85Qps7B2J3uzaQ" alt="" />
+                <div>Prashant GUPTA</div>
+              </div>
+
+              <div className='flex flex-col items-center'>
+                <img className='w-24 h-24' src="https://media.licdn.com/dms/image/C5603AQHCQsuLbeNEKw/profile-displayphoto-shrink_800_800/0/1517754283443?e=1689811200&v=beta&t=SaCZeBy4QS_hRL0xId3a8vu3icAlE85Qps7B2J3uzaQ" alt="" />
+                <div>May CHEN</div>
+              </div>
+
+              <div className='flex flex-col items-center'>
+                <img className='w-24 h-24' src="https://media.licdn.com/dms/image/C5603AQHCQsuLbeNEKw/profile-displayphoto-shrink_800_800/0/1517754283443?e=1689811200&v=beta&t=SaCZeBy4QS_hRL0xId3a8vu3icAlE85Qps7B2J3uzaQ" alt="" />
+                <div>Thomas STREATER</div>
+              </div>
+
+              <div className='flex flex-col items-center'>
+                <img className='w-24 h-24' src="https://media.licdn.com/dms/image/C5603AQHCQsuLbeNEKw/profile-displayphoto-shrink_800_800/0/1517754283443?e=1689811200&v=beta&t=SaCZeBy4QS_hRL0xId3a8vu3icAlE85Qps7B2J3uzaQ" alt="" />
+                <div>Eric YIM</div>
+              </div>
+
+              <div className='flex flex-col items-center'>
+                <img className='w-24 h-24' src="https://media.licdn.com/dms/image/C5603AQHCQsuLbeNEKw/profile-displayphoto-shrink_800_800/0/1517754283443?e=1689811200&v=beta&t=SaCZeBy4QS_hRL0xId3a8vu3icAlE85Qps7B2J3uzaQ" alt="" />
+                <div>Alex TSANG</div>
+              </div>
+
+              <div className='flex flex-col items-center'>
+                <img className='w-24 h-24' src="https://media.licdn.com/dms/image/C5603AQHCQsuLbeNEKw/profile-displayphoto-shrink_800_800/0/1517754283443?e=1689811200&v=beta&t=SaCZeBy4QS_hRL0xId3a8vu3icAlE85Qps7B2J3uzaQ" alt="" />
+                <div>Joey FAN</div>
+              </div>
+
+              <div className='flex flex-col items-center'>
+                <img className='w-24 h-24' src="https://media.licdn.com/dms/image/C5603AQHCQsuLbeNEKw/profile-displayphoto-shrink_800_800/0/1517754283443?e=1689811200&v=beta&t=SaCZeBy4QS_hRL0xId3a8vu3icAlE85Qps7B2J3uzaQ" alt="" />
+                <div>Clara CHEUNG</div>
+              </div>
+
+              <div className='flex flex-col items-center'>
+                <img className='w-24 h-24' src="https://media.licdn.com/dms/image/C5603AQHCQsuLbeNEKw/profile-displayphoto-shrink_800_800/0/1517754283443?e=1689811200&v=beta&t=SaCZeBy4QS_hRL0xId3a8vu3icAlE85Qps7B2J3uzaQ" alt="" />
+                <div>Maxim GELMANN</div>
+              </div>
+
+
+          </div>
+
+        </div>
+        
+         
+        <div></div>
+      </div>
+      
+
+
+    </div>
+  )
+}
+
+export default Test
